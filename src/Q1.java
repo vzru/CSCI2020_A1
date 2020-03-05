60d8858b5eac4ca405f54c421f16624b6b3ae092
Jakob Anderson <jakob.anderson@uoit.net>
2020-03-05 18:35:09 -0500

    Comments

--- src/Q1.java
+++ src/Q1.java
@@ -10,12 +10,15 @@ import javafx.stage.Stage;
 public class Q1  extends Application {
     @Override
     public void start(Stage stage) throws Exception {
+
+        //Create gridpane for easy ordering
         GridPane pane = new GridPane();
         pane.setAlignment(Pos.CENTER);
         pane.setPadding(new Insets(10, 10, 10, 10));
         pane.setVgap(5);
         pane.setHgap(5);
 
+        //Randomly select 3 cards in the Cards folder, add it to the pane at column 'i'
         for (int i = 0; i < 3; ++i)
         {
             int rand = (int)(Math.random() * 54) % 54;
@@ -25,6 +28,7 @@ public class Q1  extends Application {
             pane.add(new ImageView(c), i, 0);
         }
 
+        //Show scene
         Scene scene = new Scene(pane);
         stage.setScene(scene);
         stage.show();
--- src/Q3.java
+++ src/Q3.java
@@ -19,11 +19,14 @@ public class Q3  extends Application {
     @Override
     public void start(Stage stage) throws Exception {
 
+        //Create MultiCircle custom pane
         MultiCircle mainCircle = new MultiCircle(250, 20, 300,300,200,3);
 
+        //Set the big circle's color
         mainCircle.bigC.setStroke(Color.BLACK);
         mainCircle.bigC.setFill(Color.WHITE);
 
+        //Show scene
         Scene scene = new Scene(mainCircle, 600, 600);
         stage.setScene(scene);
 
@@ -36,36 +39,56 @@ public class Q3  extends Application {
     }
 }
 
+//Main class, encapsulates all functions and data
 class MultiCircle extends Pane{
 
+    //Circle being dragged
     public static ClickableCircle currentlyMoving = null;
+
+    //Main big circle
     public Circle bigC = new Circle();
+
+    //Smaller circles
     public ClickableCircle[] cc;
+
+    //Angle text
     public Text[] tc;
+
+    //Lines between circles
     public Line[] lc;
 
+    //Private variables for use inside the class
     private double br, lr, tr, cx, cy;
     private int pn;
     private int currentInArray = -1;
 
+    //constructor sets up private variables
     public MultiCircle(double bigRad, double littleRad, double centerX, double centerY, double textRad, int pointNum) {
+        //Number of points - 3 due to assignment reqs, can be more
         pn = pointNum;
 
+        //Create new array of clickable circles, angle texts and lines
+        //Array lengths are all equal to the number of points specified
         cc = new ClickableCircle[pn];
         tc = new Text[pn];
         lc = new Line[pn];
 
-        br = bigRad;
-        lr = littleRad;
-        tr = textRad;
-        cx = centerX;
-        cy = centerY;
+        //Store additional information for later use
+        br = bigRad; //Big circle radius
+        lr = littleRad; //Radius of clickable circles
+        tr = textRad; //Radius that text will be at, relative to big circle
+        cx = centerX; //Center of big circle X
+        cy = centerY; //Center of big circle Y
 
+        //Set default radius of the big circle, ass it to the pane
         bigC.setRadius(br);
         getChildren().add(bigC);
         bigC.setCenterX(cx);
         bigC.setCenterY(cy);
 
+
+        //All points start equidistant from each other.
+        //Text for default angles of points are calculated below.
         for (int i = 0; i < pn; ++i) {
             tc[i] = new Text(Double.toString((360f) / pn));
             tc[i].setX(cx + tr * Math.sin(Math.PI * 2f / pn * i));
@@ -74,6 +97,8 @@ class MultiCircle extends Pane{
             getChildren().add(tc[i]);
         }
 
+        //Lines are placed where the centerpoints of the clickable circles will be
+        //Lines go from the current circle to the one after, with the last looping back
         for (int i = 0; i < pn; ++i) {
             lc[i] = new Line(
                     cx + br * Math.sin(Math.PI * 2f / pn * i),
@@ -85,6 +110,7 @@ class MultiCircle extends Pane{
             getChildren().add(lc[i]);
         }
 
+        //Clickable circles are placed on the radius of the big circle
         for (int i = 0; i < pn; ++i) {
             cc[i] = new ClickableCircle(lr, i);
             cc[i].setCenterX(cx + br * Math.sin(Math.PI * 2f / pn * i));
@@ -92,6 +118,7 @@ class MultiCircle extends Pane{
 
             getChildren().add(cc[i]);
 
+            //The currently selected circle is set to null upon release of the mouse
             cc[i].setOnMouseReleased(e -> {
                 if (currentlyMoving == e.getSource()) {
                     currentlyMoving = null;
@@ -99,20 +126,23 @@ class MultiCircle extends Pane{
                 }
             });
 
+            //When the mouse is dragged, update positions of text, lines and circles
             cc[i].setOnMouseDragged(e -> {
+                //If there is no selected circle, set it to this circle
                 if (currentlyMoving == null) {
-                    //if (((ClickableCircle) e.getSource()).CheckIfInside(e.getX(), e.getY())) {
-                        currentlyMoving = (ClickableCircle) e.getSource();
-                        currentInArray = ((ClickableCircle) e.getSource()).arrPos;
-                    //}
+                    currentlyMoving = (ClickableCircle) e.getSource();
+                    currentInArray = ((ClickableCircle) e.getSource()).arrPos;
                 }
 
+                //If this is the selected circle, allow click and drag
                 if (currentlyMoving == e.getSource()) {
+                    //Distance between big circle center and mouse
                     double DX = e.getX() - cx;
                     double DY = e.getY() - cy;
 
                     double newRad = Math.sqrt(DX * DX + DY * DY);
 
+                    //Edge case for if that distance is 0
                     if (newRad == 0) {
                         currentlyMoving.setCenterX(cx + 0);
                         currentlyMoving.setCenterY(cy + br);
@@ -127,26 +157,35 @@ class MultiCircle extends Pane{
                         lc[currentInArray].setStartX(cx + 0);
                         lc[currentInArray].setStartY(cy + br);
                     } else {
+                        //Normalized vector coordinates, setting the magnitude
+                        //  to be equal to the big circle's radius
                         double newX = cx + DX * br / newRad;
                         double newY = cy + DY * br / newRad;
 
+                        //Set position of currently selected circle
                         currentlyMoving.setCenterX(newX);
                         currentlyMoving.setCenterY(newY);
 
+                        //Same as above but for text
                         double newTX = cx + DX * tr / newRad;
                         double newTY = cy + DY * tr / newRad;
 
                         tc[currentInArray].setX(newTX);
                         tc[currentInArray].setY(newTY);
 
+                        //Check if the circle is the first in the array, as
+                        //  the first and last line must be used then
+                        //  otherwise, it takes the current line and the previous
                         int prev = currentInArray > 0 ? currentInArray - 1 : cc.length - 1;
 
+                        //Update line start/end positions
                         lc[prev].setEndX(newX);
                         lc[prev].setEndY(newY);
                         lc[currentInArray].setStartX(newX);
                         lc[currentInArray].setStartY(newY);
                     }
 
+                    //Find lengths of relevant sides
                     int p1 = currentInArray - 2;
                     p1 = p1 < 0 ? p1 + cc.length : p1;
                     int p2 = currentInArray - 1;
@@ -166,6 +205,7 @@ class MultiCircle extends Pane{
                     double len4x = cc[p4].getCenterX() - cc[p5].getCenterX();
                     double len4y = cc[p4].getCenterY() - cc[p5].getCenterY();
 
+                    //Find lengths of sides opposite angles that need updating
                     double mid1x = cc[p1].getCenterX() - cc[p3].getCenterX();
                     double mid1y = cc[p1].getCenterY() - cc[p3].getCenterY();
                     double mid2x = cc[p2].getCenterX() - cc[p4].getCenterX();
@@ -182,6 +222,7 @@ class MultiCircle extends Pane{
                     double mid2 = Math.sqrt(mid2x * mid2x + mid2y * mid2y);
                     double mid3 = Math.sqrt(mid3x * mid3x + mid3y * mid3y);
 
+                    //Update text to be the angle, using formulas provided
                     tc[p2].setText(Double.toString(calcAngleOutput(mid1, len1, len2)));
                     tc[p3].setText(Double.toString(calcAngleOutput(mid2, len2, len3)));
                     tc[p4].setText(Double.toString(calcAngleOutput(mid3, len3, len4)));
@@ -190,6 +231,7 @@ class MultiCircle extends Pane{
         }
     }
 
+    //Formula provided
     private double calcAngleOutput(double pMid, double pLeft, double pRight)
     {
         if (2 * pLeft * pRight <= 0)
@@ -198,6 +240,7 @@ class MultiCircle extends Pane{
     }
 }
 
+//Class that extends circle, simply stores what number it is and sets defaults
 class ClickableCircle extends Circle{
 
     public int arrPos;
